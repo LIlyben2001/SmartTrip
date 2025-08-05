7720af45864e158e16ba0af1d3bb3355e57afdde
LIlyben2001 <LIlyben2001@yahoo.com>
2025-08-05 12:56:31 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,7 @@
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#1F2F46",
-        accent: "#FF6B35",
-        text: "#1F2F46",
-        background: "#FAFAFA",
-        hover: "#E65A2D"
-      }
-    },
+    extend: {},
   },
   plugins: [],
 };
