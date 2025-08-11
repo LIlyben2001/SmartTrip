db6a8c4ef362268db80b307b26a88262bce929f7
LIlyben2001 <LIlyben2001@yahoo.com>
2025-08-10 22:44:46 -0400

    remove country info

--- api/countries/route.js
+++
@@ -1,7 +0,0 @@
-export async function GET() {
-  const data = [
-    "United States","Canada","United Kingdom","France","Italy",
-    "Spain","Germany","Australia","Japan","China","Greece","Turkey"
-  ];
-  return Response.json(data);
-}
