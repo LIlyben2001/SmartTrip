9a63ed00af289889d4f864f7e839a73ea4f00e87
LIlyben2001 <LIlyben2001@yahoo.com>
2025-08-14 21:29:52 -0400

    Update send-itinerary.js

--- api/send-itinerary.js
+++ api/send-itinerary.js
@@ -1,5 +1,5 @@
 // /api/send-itinerary.js
-export default async function handler(req, res) {
+module.exports = async function (req, res) {
   // CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
