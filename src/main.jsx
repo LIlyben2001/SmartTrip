9f9361c1c275eb883a983d85b15cacbf545633be
LIlyben2001 <LIlyben2001@yahoo.com>
2025-08-17 21:10:25 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-<<<<<<< HEAD
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-=======
 import { LanguageProvider } from "./i18n";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -19,4 +11,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </LanguageProvider>
   </React.StrictMode>
 );
->>>>>>> 1074d6a388cc5c4669705f34610de80ef887fd54
