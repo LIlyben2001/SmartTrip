4ac2e96d21501ca3516b172b2d05b81e8f9d59f8
LIlyben2001 <LIlyben2001@yahoo.com>
2025-08-24 16:53:02 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import TripPlanner from "@/src/components/TripPlanner";
+import TripPlanner from "../src/components/TripPlanner";
 export default function Page() {
   return <TripPlanner />;
 }
